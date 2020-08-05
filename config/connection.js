546adf35f9f00aa375ff1ed8ba3bfc1be8c8b452
OkieKenobi <travismartinez88@gmail.comgit config --global user.name Tmartinez0202>
2020-08-04 18:37:03 -0600

    added jawsdb code

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,20 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else {
+  connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "Brandnew12!",
   database: "burgers_db"
 });
+};
 
-// Make connection.
+
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -17,5 +23,5 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
-// Export connection for our ORM to use.
+
 module.exports = connection;
